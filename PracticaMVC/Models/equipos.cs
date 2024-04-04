48645a9e1786826b75993bb74d497759a8268ddc
Diego Burgos <d.a.burgos.servellon@gmail.com>
2024-04-04 15:30:26 -0600

    Agregar archivos de proyecto.

---
+++ PracticaMVC/Models/equipos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaMVC.Models
+{
+    public class equipos
+    {
+        [Key]
+        public int id_equipo { get; set; }
+        public string? nombre { get; set; } 
+        public string? descripcion {  get; set; }
+        public int? marca_id {  get; set; }
+    }
+}
